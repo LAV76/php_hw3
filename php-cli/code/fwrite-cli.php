7f1ec4c5969b4d6bc17bbdc74dea94912aff2e82
Alexander Lekontsev <5958383@mail.ru>
2024-12-14 01:37:53 +0300

    Задание №3

--- php-cli/code/fwrite-cli.php
+++ php-cli/code/fwrite-cli.php
@@ -2,45 +2,45 @@
 
 $address = '/code/birthdays.txt';
 
+// Получаем данные от пользователя
 $name = readline("Введите имя: ");
 $date = readline("Введите дату рождения в формате ДД-ММ-ГГГГ: ");
 
-if(validate($date)){
+// Проверяем данные
+if (validateName($name) && validateDate($date)) {
     $data = $name . ", " . $date . "\r\n";
 
+    // Открываем файл и записываем данные
     $fileHandler = fopen($address, 'a');
     
-    if(fwrite($fileHandler, $data)){
-        echo "Запись $data добавлена в файл $address";
-    }
-    else {
-        echo "Произошла ошибка записи. Данные не сохранены";
+    if (fwrite($fileHandler, $data)) {
+        echo "Запись '$data' добавлена в файл $address\n";
+    } else {
+        echo "Произошла ошибка записи. Данные не сохранены.\n";
     }
     
     fclose($fileHandler);
+} else {
+    echo "Введена некорректная информация.\n";
 }
-else{
-    echo "Введена некорректная информация";
+
+// Функция проверки имени
+function validateName(string $name): bool {
+    // Имя не должно быть пустым, содержать цифры или специальные символы
+    return preg_match('/^[a-zA-Zа-яА-ЯёЁ]+(?: [a-zA-Zа-яА-ЯёЁ]+)*$/u', $name);
 }
 
-function validate(string $date): bool {
-    $dateBlocks = explode("-", $date);
+// Функция проверки даты
+function validateDate(string $date): bool {
+    // Проверяем формат даты ДД-ММ-ГГГГ
+    $format = 'd-m-Y';
+    $parsedDate = DateTime::createFromFormat($format, $date);
 
-    if(count($dateBlocks) < 3){
-        return false;
+    if ($parsedDate && $parsedDate->format($format) === $date) {
+        // Проверяем, что дата не больше текущей
+        $currentDate = new DateTime();
+        return $parsedDate <= $currentDate;
     }
 
-    if(isset($dateBlocks[0]) && $dateBlocks[0] > 31) {
-        return false;
-    }
-
-    if(isset($dateBlocks[1]) && $dateBlocks[0] > 12) {
-        return false;
-    }
-
-    if(isset($dateBlocks[2]) && $dateBlocks[2] > date('Y')) {
-        return false;
-    }
-
-    return true;
+    return false;
 }
